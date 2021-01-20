a173e1a8a80bd501855d027d087bb3802fc8c133
Felipe Xavier <felipe_rsr@live.com>
2021-01-20 13:37:52 -0300

    Update ambiente.js

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,13 +1,19 @@
-let fatias = 8;
+// let fatias = 8;
 
-while (fatias > 0) {
-  console.log("comi fatia")
-  fatias--
+// while (fatias > 0) {
+//   console.log("comi fatia")
+//   fatias--
+// }
+
+// let fila = 8
+
+// do {
+//   console.log(`fila esta em: ${fila}`)
+//   fila--
+// } while (fila > 0);
+
+
+fila = 10
+for(i = 0; i <= fila; i++){
+  console.log(`Posição na fila: ${i}`)
 }
-
-let fila = 8
-
-do {
-  console.log(`fila esta em: ${fila}`)
-  fila--
-} while (fila > 0);
