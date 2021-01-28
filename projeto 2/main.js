d99cc0f5ddacd53ec6e22948805f341e94e427bc
Felipe Xavier <felipe_rsr@live.com>
2021-01-28 14:00:18 -0300

    update

--- projeto 2/main.js
+++ projeto 2/main.js
@@ -1,23 +1,103 @@
 function verificar() {
   //Pegando os valores
-  let res = document.querySelector('#res')
-  let anoAtual = new Date().getFullYear()
-  let anoNascimento = Number(document.querySelector('#txtano').value)
-  let sexo = document.querySelector('input[name="radsex"]:checked').value
+  let res = document.querySelector('#res');
+  let anoAtual = new Date().getFullYear();
+  let anoNascimento = Number(document.querySelector('#txtano').value);
+  let sexo = Number(document.querySelector('input[name="radsex"]:checked').value);
+  genero = '';
+  let img = document.createElement('img');
+  img.setAttribute('id', 'foto');
+
+
+  //Calcular idade
+  let idade =  anoAtual - anoNascimento;
+
+  if (anoNascimento == 0 || anoNascimento > anoAtual) {
+    res.innerHTML = `<p>[ERRO!] Verifique o ano de nascimento</p>`
+  } else {
+    if (sexo == 1) {
+      genero = 'Masculino'
+      if (idade >= 0 && idade < 10) { 
+        //criança
+        img.setAttribute('src', 'img/crianca-homem.jpg')
+      } else if (idade < 18){
+        //jovem
+        img.setAttribute('src', 'img/jovem-homem.jpg')
+      } else if(idade < 50) {
+        //adulto
+      }else{
+        //idoso
+      }
+    } else if (sexo == 2){
+        genero = 'Feminino'
+        if (idade >= 0 && idade < 10) { 
+          //criança
+        } else if (idade < 18){
+          //jovem
+        } else if(idade < 50) {
+          //adulta
+        }else{
+          //idosa
+        }
+      }
+      res.innerHTML = `<p>Detectamos ${genero} de ${idade} anos</p>`;
+      res.appendChild(img);
+    }
+
+}
+
 
 
   //verificar se o ano é valido
+  /*
   if (anoNascimento == 0 || anoNascimento > anoAtual) {
-    res.innerHTML = `<p>Verifique o ano de nascimento</p>`
-  }
-
-  //Calcular idade
-  let idade = anoNascimento - anoAtual
-
-  //verificar faixa etaria
-  if (idade <= 12 ) {
-    res.innerHTML = `<div class='teste'>  <img src="img/crianca-homem.jpg" alt="crianca-homem">`
-  } else {
-    
-  }
-}
+    res.innerHTML = `<p>[ERRO]Verifique o ano de nascimento</p>`
+  }else if (idade <= 12 && sexo == 1) { //verificar faixa etaria
+    res.innerHTML = `
+    <div class='teste'> 
+    <h3>Olá, é uma criança masculina de ${idade} anos</h3>
+    <img src="img/crianca-homem.jpg" alt="crianca-homem">
+    `
+  } else if (idade <= 12 && sexo == 2){
+    res.innerHTML = `
+    <div class='teste'> 
+    <h3>Olá, é uma criança feminina de ${idade} anos</h3>
+    <img src="img/crianca-mulher.jpg" alt="crianca-homem">
+    `
+  }else if (idade < 18 && sexo == 1){
+    res.innerHTML = `
+    <div class='teste'> 
+    <h3>Olá, é um jovem masculino de ${idade} anos</h3>
+    <img src="img/jovem-homem.jpg" alt="jovem-homem">
+    `
+  }else if (idade < 18 && sexo == 2){
+    res.innerHTML = `
+    <div class='teste'> 
+    <h3>Olá, é uma jovem feminina de ${idade} anos</h3>
+    <img src="img/jovem-mulher.jpg" alt="jovem-mulher">
+    `
+  }else if (idade < 48 && sexo == 1){
+    res.innerHTML = `
+    <div class='teste'> 
+    <h3>Olá, é um Adulto masculino de ${idade} anos</h3>
+    <img src="img/adulto-homem.jpg" alt="adulto-homem">
+    `
+  }else if (idade < 48 && sexo == 2){
+    res.innerHTML = `
+    <div class='teste'> 
+    <h3>Olá, é uma Adulta feminina de ${idade} anos</h3>
+    <img src="img/adulta-mulher.jpg" alt="adulto-mulher">
+    `
+  }else if (idade > 48 && sexo == 1){
+    res.innerHTML = `
+    <div class='teste'> 
+    <h3>Olá, é um idoso masculino de ${idade} anos</h3>
+    <img src="img/idoso-homem.jpg" alt="idoso-homem">
+    `
+  }else if (idade > 48 && sexo == 2){
+    res.innerHTML = `
+    <div class='teste'> 
+    <h3>Olá, é uma idoso masculino de ${idade} anos</h3>
+    <img src="img/idoso-mulher.jpg" alt="idoso-mulher">
+    `
+  }*/
