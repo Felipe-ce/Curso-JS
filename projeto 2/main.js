d08abcf7ae16fd7cb109267e80b7ea98de062d49
Felipe Xavier <felipe_rsr@live.com>
2021-01-28 09:58:56 -0300

    Update

--- projeto 2/main.js
+++ projeto 2/main.js
@@ -3,10 +3,21 @@ function verificar() {
   let res = document.querySelector('#res')
   let anoAtual = new Date().getFullYear()
   let anoNascimento = Number(document.querySelector('#txtano').value)
-  //let sexo = document.querySelector('#')
+  let sexo = document.querySelector('input[name="radsex"]:checked').value
+
 
   //verificar se o ano é valido
   if (anoNascimento == 0 || anoNascimento > anoAtual) {
     res.innerHTML = `<p>Verifique o ano de nascimento</p>`
+  }
+
+  //Calcular idade
+  let idade = anoNascimento - anoAtual
+
+  //verificar faixa etaria
+  if (idade <= 12 ) {
+    res.innerHTML = `<div class='teste'>  <img src="img/crianca-homem.jpg" alt="crianca-homem">`
+  } else {
+    
   }
 }
