1a257ba0d551dbebc1d16d75b6f6983a55610cd0
Felipe-CE <felipe_rsr@live.com>
2021-01-28 22:48:56 -0300

    Final projeto 3

--- projeto 3/main.js
+++ projeto 3/main.js
@@ -2,7 +2,7 @@ function contar() {
   //pegar valores do input
   let ini = document.querySelector('#txti')
   let fim = document.querySelector('#txtf')
-  let passo = document.querySelector('#passo')
+  let passo = document.querySelector('#txtp')
   
   //pegando a div res
   let res = document.querySelector('.res')
@@ -10,11 +10,25 @@ function contar() {
   if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
     res.innerHTML = `<p>[ERRO!] Verifique os dados</p>`
   } else {
-    res.innerHTML = `Contando: ...`
+    res.innerHTML = `<p>Contando:</p>`
     i = Number(ini.value)
     f = Number(fim.value)
     p = Number(passo.value)
-    console.log(i, f ,p)
+    if (p <= 0) {
+      res.innerHTML = `<p>Passo tem que ser maior que zero, considerando PASSO = 1:</p>`
+      p = 1;
+    }
 
+    if (i < f) {
+      //Contagem crescente
+      for(let c = i; c <= f; c += p){
+        res.innerHTML += `<span>${c}</span>, `
+      }
+    } else {
+      //Contagem crescente
+      for(let c = i; c >= f; c -= p){
+        res.innerHTML += `<span>${c}</span>, `
+      }
+    }
   }
 }
